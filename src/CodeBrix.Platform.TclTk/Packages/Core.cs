b1df285f7fbb936e5463aaf06db200e8362a8fda
Jeremy Ellis <jeremy@ellisnet.com>
2026-07-15 12:31:05 -0700

    Bump nugets on samples. Include packages.

---
+++ src/CodeBrix.Platform.TclTk/Packages/Core.cs
@@ -0,0 +1,250 @@
+/*
+ * Core.cs --
+ *
+ * Copyright (c) 2007-2012 by Joe Mistachkin.  All rights reserved.
+ *
+ * See the file "license.terms" for information on usage and redistribution of
+ * this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * RCS: @(#) $Id: $
+ */
+
+using System;
+using CodeBrix.Platform.TclTk._Attributes;
+using CodeBrix.Platform.TclTk._Components.Private;
+using CodeBrix.Platform.TclTk._Components.Public;
+using CodeBrix.Platform.TclTk._Containers.Public;
+using CodeBrix.Platform.TclTk._Interfaces.Public;
+
+namespace CodeBrix.Platform.TclTk._Packages //was previously: Eagle._Packages;
+{
+    /// <summary>
+    /// This class implements the core package used by the TclTk engine to
+    /// represent a script package.  It derives from <see cref="Default" /> and
+    /// provides working implementations of version selection and package
+    /// loading, evaluating the appropriate <c>ifneeded</c> script to satisfy a
+    /// [package require] request.  See <c>core_language.md</c> for package
+    /// management semantics.
+    /// </summary>
+    [ObjectId("c0e022cd-2c9b-4020-9d27-312eef08a3cd")]
+    public class Core : Default
+    {
+        #region Public Constructors
+        /// <summary>
+        /// Constructs an instance of the core package.
+        /// </summary>
+        /// <param name="packageData">
+        /// The data used to create and identify this package, such as its name
+        /// and the set of available <c>ifneeded</c> scripts.  This parameter
+        /// may be null.
+        /// </param>
+        public Core(
+            IPackageData packageData
+            )
+            : base(packageData)
+        {
+            //
+            // NOTE: Which package are we actively trying to load?  This is an
+            //       internal implementation detail and is not exposed via the
+            //       IPackage interface.
+            //
+            loading = null;
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IPackage Members
+        /// <summary>
+        /// Selects a version of this package to satisfy a request, according
+        /// to the supplied preference.
+        /// </summary>
+        /// <param name="preference">
+        /// The preference that governs how a candidate version is chosen.
+        /// </param>
+        /// <param name="version">
+        /// Upon success, receives the selected version of this package.
+        /// </param>
+        /// <param name="error">
+        /// Upon failure, receives an appropriate error message.
+        /// </param>
+        /// <returns>
+        /// <see cref="ReturnCode.Ok" /> on success; otherwise,
+        /// <see cref="ReturnCode.Error" /> with details placed in the
+        /// <paramref name="error" /> parameter.
+        /// </returns>
+        public override ReturnCode Select(
+            PackagePreference preference,
+            ref Version version,
+            ref Result error
+            )
+        {
+            if (preference == PackagePreference.Default)
+            {
+                string name = this.Name;
+                VersionStringDictionary ifNeeded = this.IfNeeded;
+
+                if (ifNeeded != null)
+                {
+                    //
+                    // NOTE: *HACK* For now, always select the latest version
+                    //       from the list of candidate versions.
+                    //
+                    Version latest = null;
+
+                    foreach (Version candidate in ifNeeded.Keys)
+                        if (PackageOps.VersionCompare(candidate, latest) > 0)
+                            latest = candidate;
+
+                    //
+                    // NOTE: Were we able to find the latest (i.e. any)
+                    //       version?
+                    //
+                    if (latest != null)
+                    {
+                        version = latest;
+                        return ReturnCode.Ok;
+                    }
+                    else
+                    {
+                        error = String.Format(
+                            "can't find package {0}",
+                            FormatOps.PackageName(name, null));
+                    }
+                }
+                else
+                {
+                    error = String.Format(
+                        "package {0} ifneeded scripts not available",
+                        FormatOps.WrapOrNull(name));
+                }
+            }
+            else
+            {
+                error = String.Format(
+                    "unsupported package preference {0}",
+                    FormatOps.WrapOrNull(preference));
+            }
+
+            return ReturnCode.Error;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The version of this package that is currently being loaded, if any;
+        /// used to detect circular package dependencies.  This is an internal
+        /// implementation detail and is not exposed via the IPackage interface.
+        /// </summary>
+        private Version loading; // which version are we actively loading?
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Loads the specified version of this package by evaluating its
+        /// corresponding <c>ifneeded</c> script.  Circular package
+        /// dependencies are detected and reported as an error.
+        /// </summary>
+        /// <param name="interpreter">
+        /// The interpreter context in which to evaluate the package script.
+        /// This parameter should not be null.
+        /// </param>
+        /// <param name="version">
+        /// The version of this package to load.  This parameter should not be
+        /// null.
+        /// </param>
+        /// <param name="result">
+        /// Upon success, this may contain the result produced by evaluating
+        /// the package script.  Upon failure, this must contain an appropriate
+        /// error message.
+        /// </param>
+        /// <returns>
+        /// <see cref="ReturnCode.Ok" /> on success; otherwise, a non-Ok value
+        /// with details placed in the <paramref name="result" /> parameter.
+        /// </returns>
+        public override ReturnCode Load(
+            Interpreter interpreter,
+            Version version,
+            ref Result result
+            )
+        {
+            if (interpreter != null)
+            {
+                if (version != null)
+                {
+                    string name = this.Name;
+                    VersionStringDictionary ifNeeded = this.IfNeeded;
+
+                    if (ifNeeded != null)
+                    {
+                        string text;
+
+                        if (ifNeeded.TryGetValue(version, out text))
+                        {
+                            if (!FlagOps.HasFlags(Flags, PackageFlags.Loading, true))
+                            {
+                                Flags |= PackageFlags.Loading;
+                                loading = version;
+
+                                try
+                                {
+                                    ReturnCode code;
+
+                                    code = interpreter.EvaluatePackageScript(
+                                        text, ref result);
+
+                                    if (code == ReturnCode.Ok)
+                                        WasNeeded = text;
+
+                                    return code;
+                                }
+                                catch (Exception e)
+                                {
+                                    result = String.Format(
+                                        "caught exception while evaluating ifneeded script: {0}",
+                                        e);
+                                }
+                                finally
+                                {
+                                    loading = null;
+                                    Flags &= ~PackageFlags.Loading;
+                                }
+                            }
+                            else
+                            {
+                                result = String.Format(
+                                    "circular package dependency: " +
+                                    "attempt to provide {0} requires {1}",
+                                    FormatOps.PackageName(name, version),
+                                    FormatOps.PackageName(name, loading));
+                            }
+                        }
+                        else
+                        {
+                            result = String.Format(
+                                "can't find package {0}",
+                                FormatOps.PackageName(name, version));
+                        }
+                    }
+                    else
+                    {
+                        result = String.Format(
+                            "package {0} ifneeded scripts not available",
+                            FormatOps.WrapOrNull(name));
+                    }
+                }
+                else
+                {
+                    result = "invalid package version";
+                }
+            }
+            else
+            {
+                result = "invalid interpreter";
+            }
+
+            return ReturnCode.Error;
+        }
+        #endregion
+    }
+}
---
+++ src/CodeBrix.Platform.TclTk/Packages/Default.cs
@@ -0,0 +1,463 @@
+/*
+ * Default.cs --
+ *
+ * Copyright (c) 2007-2012 by Joe Mistachkin.  All rights reserved.
+ *
+ * See the file "license.terms" for information on usage and redistribution of
+ * this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * RCS: @(#) $Id: $
+ */
+
+using System;
+using System.Threading;
+using CodeBrix.Platform.TclTk._Attributes;
+using CodeBrix.Platform.TclTk._Components.Private;
+using CodeBrix.Platform.TclTk._Components.Public;
+using CodeBrix.Platform.TclTk._Containers.Public;
+using CodeBrix.Platform.TclTk._Interfaces.Public;
+
+namespace CodeBrix.Platform.TclTk._Packages //was previously: Eagle._Packages;
+{
+    /// <summary>
+    /// This class provides the default implementation of the
+    /// <see cref="IPackage" /> interface, which represents a script package
+    /// within the TclTk engine.  It supplies the common identity, state, and
+    /// package-data storage (including the set of available <c>ifneeded</c>
+    /// scripts) shared by all packages; its <see cref="Select" /> and
+    /// <see cref="Load" /> methods are no-op placeholders that derived classes
+    /// override.  See <c>core_language.md</c> for package management
+    /// semantics.
+    /// </summary>
+    [ObjectId("d97bbc96-0d1e-4263-82cd-f963ddb3f6ac")]
+    public class Default : IPackage
+    {
+        #region Public Constructors
+        /// <summary>
+        /// Constructs an instance of the default package, optionally
+        /// initializing it from the supplied package data.
+        /// </summary>
+        /// <param name="packageData">
+        /// The data used to create and identify this package, such as its name
+        /// and the set of available <c>ifneeded</c> scripts.  This parameter
+        /// may be null, in which case the package is left with default property
+        /// values.
+        /// </param>
+        public Default(
+            IPackageData packageData
+            )
+        {
+            kind = IdentifierKind.Package;
+
+            if ((packageData == null) ||
+                !FlagOps.HasFlags(packageData.Flags,
+                    PackageFlags.NoAttributes, true))
+            {
+                id = AttributeOps.GetObjectId(this);
+                group = AttributeOps.GetObjectGroups(this);
+            }
+
+            if (packageData != null)
+            {
+                id = packageData.Id;
+
+                EntityOps.MaybeSetupId(this);
+
+                EntityOps.MaybeSetGroup(
+                    this, packageData.Group);
+
+                name = packageData.Name;
+                description = packageData.Description;
+                indexFileName = packageData.IndexFileName;
+                provideFileName = packageData.ProvideFileName;
+                flags = packageData.Flags;
+                clientData = packageData.ClientData;
+                loaded = packageData.Loaded;
+
+                VersionStringDictionary ifNeeded = packageData.IfNeeded;
+
+                if (ifNeeded != null)
+                    this.ifNeeded = ifNeeded; // use (or "attach to") their versions.
+                else
+                    this.ifNeeded = new VersionStringDictionary(); // brand new package, create new list.
+
+                token = packageData.Token;
+            }
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IIdentifierName Members
+        /// <summary>
+        /// The name of this package.
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// Gets or sets the name of this package.
+        /// </summary>
+        public virtual string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IIdentifierBase Members
+        /// <summary>
+        /// The kind of identifier represented by this package.
+        /// </summary>
+        private IdentifierKind kind;
+        /// <summary>
+        /// Gets or sets the kind of identifier represented by this package.
+        /// </summary>
+        public virtual IdentifierKind Kind
+        {
+            get { return kind; }
+            set { kind = value; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The unique identifier for this package.
+        /// </summary>
+        private Guid id;
+        /// <summary>
+        /// Gets or sets the unique identifier for this package.
+        /// </summary>
+        public virtual Guid Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IGetClientData / ISetClientData Members
+        /// <summary>
+        /// The extra, caller-specific data associated with this package, if
+        /// any.
+        /// </summary>
+        private IClientData clientData;
+        /// <summary>
+        /// Gets or sets the extra, caller-specific data associated with this
+        /// package.
+        /// </summary>
+        public virtual IClientData ClientData
+        {
+            get { return clientData; }
+            set { clientData = value; }
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IIdentifier Members
+        /// <summary>
+        /// The name of the group this package belongs to, if any.
+        /// </summary>
+        private string group;
+        /// <summary>
+        /// Gets or sets the name of the group this package belongs to.
+        /// </summary>
+        public virtual string Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The human-readable description of this package, if any.
+        /// </summary>
+        private string description;
+        /// <summary>
+        /// Gets or sets the human-readable description of this package.
+        /// </summary>
+        public virtual string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IState Members
+        /// <summary>
+        /// The net number of times this package has been initialized; greater
+        /// than zero indicates the package is currently initialized.
+        /// </summary>
+        private int initializeCount;
+        /// <summary>
+        /// Gets or sets a value indicating whether this package is currently
+        /// initialized.  Setting this property to true increments, and false
+        /// decrements, the internal initialization count.
+        /// </summary>
+        public virtual bool Initialized
+        {
+            get
+            {
+                return Interlocked.CompareExchange(
+                    ref initializeCount, 0, 0) > 0;
+            }
+            set
+            {
+                if (value)
+                    Interlocked.Increment(ref initializeCount);
+                else
+                    Interlocked.Decrement(ref initializeCount);
+            }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Initializes this package, incrementing its internal initialization
+        /// count.  This default implementation always succeeds.
+        /// </summary>
+        /// <param name="interpreter">
+        /// The interpreter context this package is being initialized in.  This
+        /// parameter should not be null.
+        /// </param>
+        /// <param name="clientData">
+        /// The extra, caller-specific data supplied for this operation, if
+        /// any.  This parameter may be null.
+        /// </param>
+        /// <param name="result">
+        /// Upon failure, receives an appropriate error message.
+        /// </param>
+        /// <returns>
+        /// <see cref="ReturnCode.Ok" /> on success; otherwise, a non-Ok value
+        /// with details placed in the <paramref name="result" /> parameter.
+        /// </returns>
+        public virtual ReturnCode Initialize(
+            Interpreter interpreter,
+            IClientData clientData,
+            ref Result result
+            )
+        {
+            Interlocked.Increment(ref initializeCount);
+            return ReturnCode.Ok;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Terminates this package, decrementing its internal initialization
+        /// count.  This default implementation always succeeds.
+        /// </summary>
+        /// <param name="interpreter">
+        /// The interpreter context this package is being terminated in.  This
+        /// parameter should not be null.
+        /// </param>
+        /// <param name="clientData">
+        /// The extra, caller-specific data supplied for this operation, if
+        /// any.  This parameter may be null.
+        /// </param>
+        /// <param name="result">
+        /// Upon failure, receives an appropriate error message.
+        /// </param>
+        /// <returns>
+        /// <see cref="ReturnCode.Ok" /> on success; otherwise, a non-Ok value
+        /// with details placed in the <paramref name="result" /> parameter.
+        /// </returns>
+        public virtual ReturnCode Terminate(
+            Interpreter interpreter,
+            IClientData clientData,
+            ref Result result
+            )
+        {
+            Interlocked.Decrement(ref initializeCount);
+            return ReturnCode.Ok;
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IPackageData Members
+        /// <summary>
+        /// The name of the package index file that defined this package, if
+        /// any.
+        /// </summary>
+        private string indexFileName;
+        /// <summary>
+        /// Gets or sets the name of the package index file that defined this
+        /// package.
+        /// </summary>
+        public virtual string IndexFileName
+        {
+            get { return indexFileName; }
+            set { indexFileName = value; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The name of the file that provided this package, if any.
+        /// </summary>
+        private string provideFileName;
+        /// <summary>
+        /// Gets or sets the name of the file that provided this package.
+        /// </summary>
+        public virtual string ProvideFileName
+        {
+            get { return provideFileName; }
+            set { provideFileName = value; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The flags that control the behavior of this package.
+        /// </summary>
+        private PackageFlags flags;
+        /// <summary>
+        /// Gets or sets the flags that control the behavior of this package.
+        /// </summary>
+        public virtual PackageFlags Flags
+        {
+            get { return flags; }
+            set { flags = value; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The version of this package that is currently loaded, if any.
+        /// </summary>
+        private Version loaded;
+        /// <summary>
+        /// Gets or sets the version of this package that is currently loaded.
+        /// </summary>
+        public virtual Version Loaded
+        {
+            get { return loaded; }
+            set { loaded = value; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The collection of <c>ifneeded</c> scripts for this package, keyed
+        /// by version.
+        /// </summary>
+        private VersionStringDictionary ifNeeded;
+        /// <summary>
+        /// Gets or sets the collection of <c>ifneeded</c> scripts for this
+        /// package, keyed by version.
+        /// </summary>
+        public virtual VersionStringDictionary IfNeeded
+        {
+            get { return ifNeeded; }
+            set { ifNeeded = value; }
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// The <c>ifneeded</c> script that was actually evaluated to load this
+        /// package, if any.
+        /// </summary>
+        private string wasNeeded;
+        /// <summary>
+        /// Gets or sets the <c>ifneeded</c> script that was actually evaluated
+        /// to load this package.
+        /// </summary>
+        public virtual string WasNeeded
+        {
+            get { return wasNeeded; }
+            set { wasNeeded = value; }
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IWrapperData Members
+        /// <summary>
+        /// The interpreter token that uniquely identifies this package within
+        /// its containing collection.
+        /// </summary>
+        private long token;
+        /// <summary>
+        /// Gets or sets the interpreter token that uniquely identifies this
+        /// package within its containing collection.
+        /// </summary>
+        public virtual long Token
+        {
+            get { return token; }
+            set { token = value; }
+        }
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////////
+
+        #region IPackage Members
+        /// <summary>
+        /// Selects a version of this package to satisfy a request, according
+        /// to the supplied preference.  This default implementation performs
+        /// no work and always succeeds; derived classes override it to perform
+        /// the actual selection.
+        /// </summary>
+        /// <param name="preference">
+        /// The preference that governs how a candidate version is chosen.
+        /// </param>
+        /// <param name="version">
+        /// Upon success, receives the selected version of this package.
+        /// </param>
+        /// <param name="error">
+        /// Upon failure, receives an appropriate error message.
+        /// </param>
+        /// <returns>
+        /// <see cref="ReturnCode.Ok" /> on success; otherwise, a non-Ok value
+        /// with details placed in the <paramref name="error" /> parameter.
+        /// </returns>
+        public virtual ReturnCode Select(
+            PackagePreference preference,
+            ref Version version,
+            ref Result error
+            )
+        {
+            return ReturnCode.Ok;
+        }
+
+        ///////////////////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Loads the specified version of this package.  This default
+        /// implementation performs no work and always succeeds; derived
+        /// classes override it to perform the actual loading.
+        /// </summary>
+        /// <param name="interpreter">
+        /// The interpreter context in which to load the package.  This
+        /// parameter should not be null.
+        /// </param>
+        /// <param name="version">
+        /// The version of this package to load.  This parameter should not be
+        /// null.
+        /// </param>
+        /// <param name="result">
+        /// Upon success, this may contain the result produced while loading
+        /// the package.  Upon failure, this must contain an appropriate error
+        /// message.
+        /// </param>
+        /// <returns>
+        /// <see cref="ReturnCode.Ok" /> on success; otherwise, a non-Ok value
+        /// with details placed in the <paramref name="result" /> parameter.
+        /// </returns>
+        public virtual ReturnCode Load(
+            Interpreter interpreter,
+            Version version,
+            ref Result result
+            )
+        {
+            return ReturnCode.Ok;
+        }
+        #endregion
+    }
+}
